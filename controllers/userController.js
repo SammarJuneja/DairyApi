46e6f64e19295420b607dbd25e6fdbe21381eda4
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-11-26 23:33:34 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ exports.register = async (req, res) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password) {
-      res.status(402).json({
+      res.status(400).json({
         message: "Please fill all the details"
       });
     }
@@ -16,7 +16,7 @@ exports.register = async (req, res) => {
     });
 
     if (user) {
-      res.status(402).json({
+      res.status(403).json({
         message: "User with provided email already exists"
       });
     }
@@ -30,6 +30,35 @@ exports.register = async (req, res) => {
     res.status(200).json({
       message: "Your account was created successfully"
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "Internal server error"
+    });
+  }
+}
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      res.status(400).json({
+        message: "Please fill all the details"
+      });
+    }
+
+    const user = await User.findOne({
+      username: username
+    });
+
+    if (!user) {
+      res.status(404).json({
+        message: "Account does not exist"
+      });
+    }
+
+    
   } catch (error) {
     console.log(error);
     res.status(500).json({
