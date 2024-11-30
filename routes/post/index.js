7876ecfaab4f3b65089e71b41d183b542b9f1a85
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-11-30 23:54:16 +0530

    Update index.js

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,10 +2,6 @@ import { Router } from "express";
 import { read, write } from "../../controllers/postController.js";
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ "message": "Post routing" });
-});
-
 router.get("/get", read);
 
 export { router };
