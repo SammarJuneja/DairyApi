3e6969d41306354d751b7acf3a658bc8caef0075
Sammar Juneja <70101180+SammarJuneja@users.noreply.github.com>
2024-12-02 23:48:14 +0530

    Update index.js

--- routes/user/index.js
+++ routes/user/index.js
@@ -2,8 +2,4 @@ import { Router } from "express";
 import {  } from "../../controllers/userController.js";
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ "message": "User routing" });
-});
-
 export { router };
